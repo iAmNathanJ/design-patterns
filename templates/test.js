5e5011852360a55ac75b949ad16765beaba86782
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-22 12:42:44 -0500

    update templates/test.js: add setup/teardown

--- templates/test.js
+++ templates/test.js
@@ -1,6 +1,12 @@
 import test from 'ava';
 import <%= _.camelCase(pattern) %> from './<%= _.kebabCase(pattern) %>.js';
 
-test('<%= pattern %> is a thing', t => {
+test.before(t => {
+});
 
+test.after(t => {
+});
+
+test('<%= pattern %> is a thing', t => {
+  t.ok(<%= _.camelCase(pattern) %>);
 });
