de89f1aff3a3a87527fd4320ce5c6c569bddcbce
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-29 15:51:22 -0500

    update observer/js/index.js: begin implementation

--- observer/js/index.js
+++ observer/js/index.js
@@ -1,3 +1,16 @@
 'use strict';
 
-const observer = require('./observer.js');
+const Observer = require('./observer.js');
+
+let subject = Observer.subject();
+let observer = Observer.observer;
+
+
+subject.add(observer(e => {
+  console.log(e);
+}));
+
+subject.notify({
+  type: 'basicEvent',
+  data: 'abcdefg'
+});
