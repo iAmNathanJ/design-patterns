6d861724bfe1d53343998d3837a7f62d8d9c16c7
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-29 15:44:19 -0500

    :white_check_mark: update observer tests

--- observer/js/test.js
+++ observer/js/test.js
@@ -13,24 +13,77 @@ test('observer provides a subject', t => {
 });
 
 test('subject has observers', t => {
-  let subject = Observer.subject;
+  let subject = Observer.subject();
   t.ok(subject.observers, 'observers ok');
   t.end();
 });
 
 test('subject can add observers', t => {
-  let subject = Observer.subject;
-  let observer = Observer.observer;
-  subject.add(observer(function(e) {
-    console.log(e);
-  }));
+  let subject = Observer.subject();
+  let observer = Observer.observer(e => null);
+  subject.add(observer);
   t.equal(subject.observers.length, 1, 'observers added');
   t.end();
 });
 
 test('cannot set observers directly', t => {
+  let subject = Observer.subject();
   t.throws(() => {
-    suject.observers[0] = 'nope';
+    suject.observers = 'nope';
   });
+  t.end();
+});
+
+test('subject notifies all observers', t => {
+
+  let testVar;
+  let testVar2;
+
+  let subject = Observer.subject();
+  let observers = [
+    Observer.observer(e => {
+      testVar = 'hello';
+    }),
+    Observer.observer(e => {
+      testVar2 = 'goodbye';
+    })
+  ];
+  subject.add(observers);
+  subject.notify();
+
+  t.equal(testVar, 'hello', 'observer updated');
+  t.equal(testVar2, 'goodbye', 'observer updated');
+  t.end();
+});
+
+test('subject notifies all observers with data', t => {
+
+  let testVar = 'hello';
+  let testVar2 = 'hi there';
+
+  let subject = Observer.subject();
+  let observers = [
+    Observer.observer(e => {
+      testVar = e;
+    }),
+    Observer.observer(e => {
+      testVar2 = e;
+    })
+  ];
+  subject.add(observers);
+  subject.notify('goodbye');
+
+  t.equal(testVar, 'goodbye', 'observer updated');
+  t.equal(testVar2, 'goodbye', 'observer updated');
+  t.end();
+});
+
+test('subject removes observers', t => {
+  let subject = Observer.subject();
+  let observer = Observer.observer(e => null);
+  subject.add(observer);
+  subject.remove(observer);
+
+  t.equal(subject.observers.length, 0, 'observer removed');
   t.end();
 });
