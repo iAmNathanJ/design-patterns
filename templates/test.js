e0eb26af31fa254358416904d04e41e31d1d380a
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-29 14:33:58 -0500

    Update templates/test.js remove tape.plan(), add tape.end()

--- templates/test.js
+++ templates/test.js
@@ -3,6 +3,6 @@ const test = require('tape');
 const <%= _.camelCase(pattern) %> = require('./<%= _.kebabCase(pattern) %>.js');
 
 test('<%= pattern %> is a thing', t => {
-  t.plan(1);
   t.ok(<%= _.camelCase(pattern) %>);
+  t.end();
 });
